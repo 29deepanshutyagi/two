18ed3e2de2803eadcfb0e2eb0cc79175e042cd74
Deepanshu Tyagi <29deepanshutyagi.com>
2024-02-24 23:01:06 +0530

    video app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,74 +1,103 @@
-import React from 'react'
+import React from 'react';
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Button,
-    useDisclosure,
-    VStack,
-    HStack
-  } from '@chakra-ui/react';
-  import {Link} from "react-router-dom";
-  import {BiMenuAltLeft} from "react-icons/bi";
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
+
 const Header = () => {
-    const {isOpen,onOpen,onClose}=useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
-    <Button 
-    pos={"fixed"} 
-    top={"4"} 
-    left={"4"} 
-    colorScheme="purple" 
-    p={"0"} 
-    w={'10'}
-    h={"10"}
-    borderRadius={"full"}
-    onClick={onOpen}>
-     <BiMenuAltLeft size={'20'}/>
-    </Button>
-    <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-      
-        <DrawerOverlay/>
-        <div>sad sad</div>
+      <Button
+        zIndex={'overlay'}
+        pos={'fixed'}
+        top={'4'}
+        left={'4'}
+        colorScheme="purple"
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
+
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+
         <DrawerContent>
-            <DrawerCloseButton/>
-        <DrawerHeader>VideoHub</DrawerHeader>
-        <DrawerBody>
-            <VStack alignItems={"flex-start"}>
-                <Button variant={"ghost"} colorScheme={"purple"}>
-                    <Link to={"/"}>Home</Link>
-                </Button>
-                <Button variant={"ghost"} colorScheme={"purple"}>
-                    <Link to={"/videos"}>Videos</Link>
-                </Button>
-                <Button variant={"ghost"} colorScheme={"purple"}> 
-                    <Link to={"/videos?category=free"}>Free Videos</Link>
-                </Button>
-                <Button variant={"ghost"} colorScheme={"purple"}>
-                    <Link to={"/upload"}>Upload Videos</Link>
-                </Button>
-                
+          <DrawerCloseButton />
+          <DrawerHeader>VIDEO HUB</DrawerHeader>
+          <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/'}>Home</Link>
+              </Button>
 
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/videos?category=free'}>Free Videos</Link>
+              </Button>
+
+              <Button
+                onClick={onClose}
+                variant={'ghost'}
+                colorScheme={'purple'}
+              >
+                <Link to={'/upload'}>Upload Video</Link>
+              </Button>
             </VStack>
-            <HStack pos={"absolute"} bottom={"10"} left={"'0"}  w={"full"} justifyContent={"space-evenly"}>
-                <Button colorScheme={"purple"}>
-                    <Link>Login</Link>
-                </Button>
-                <Button colorScheme={"purple"} variant={"outline"}> 
-                    <Link>Sign up</Link>
-                </Button>
+
+            <HStack
+              pos={'absolute'}
+              bottom={'10'}
+              left={'0'}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme={'purple'}>
+                <Link to={'/login'}>Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                colorScheme={'purple'}
+                variant={'outline'}
+              >
+                <Link to={'/signup'}>Sign Up</Link>
+              </Button>
             </HStack>
-        </DrawerBody>
+          </DrawerBody>
         </DrawerContent>
-    </Drawer>
-
-
+      </Drawer>
     </>
+  );
+};
 
-  )
-}
-export default Header
+export default Header;
